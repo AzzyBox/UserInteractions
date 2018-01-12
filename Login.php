2b666473723b0350882af65bc4ffeaf579b633a1
Skinner, Adam <st20093383@outlook.cardiffmet.ac.uk>
2018-01-12 22:47:16 +0000

    Created project

---
+++ Login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adam
+ * Date: 12/01/2018
+ * Time: 22:17
+ */
--- index.php
+++ index.php
@@ -6,13 +6,78 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.css">
+
 </head>
 <body>
 
+<nav class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">User Interactions</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="Login">Login/Register</a></li>
+            </ul>
+        </div><!-- /.nav-collapse -->
+    </div><!-- /.container -->
+</nav>
+<br> <br>
+<div class="container">
+
+    <header>
+        <h1>City Gallery</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><img src="images/img1.jpg" alt="HTML5 Icon"></li>
+            <li><img src="images/img2.jpeg" alt="HTML5 Icon"></li>
+            <li><img src="images/img3.jpg" alt="HTML5 Icon"></li>
+        </ul>
+    </nav>
+
+    <article>
+        <h1>London</h1>
+        <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+        <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+    </article>
+
+    <footer>Copyright: UserInteractionsTM Example Text </footer>
+
+</div>
+
+
+
+
+<footer>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+</footer>
 </body>
-</html> <3
+</html>
 
 <!--To commit a piece of work and update what i have done to the github storebase.
 I press Ctrl + K and this automatically pushes my work to the github repsoitory -->
 
 <!--Ctrl + / to make a comment-->
+
+
+<!-- Latest compiled and minified JavaScript -->
