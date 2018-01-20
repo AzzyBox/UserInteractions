c76347bf570b2f120f64a4d0d24308fac01c5af5
Skinner, Adam <st20093383@outlook.cardiffmet.ac.uk>
2018-01-20 22:36:09 +0000

    Blah blah blah

--- index.php
+++ index.php
@@ -34,5 +34,6 @@ include 'includes/nav.php'
     I press Ctrl + K and this automatically pushes my work to the github repsoitory -->
 
     <!--Ctrl + / to make a comment-->
-    <!-- Latest compiled and minified JavaScript -->
+
+<!-- Latest compiled and minified JavaScript -->
     <!-- Latest compiled and minified JavaScript -->
